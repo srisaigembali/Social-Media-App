ae74e24da599e4a1eb42d68cc5bb5cb11271be1f
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2024-10-07 12:41:23 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import "./global.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
+const App = () => {
+	return (
+		<main className='flex h-screen'>
+			<Routes>
+				<Route element={<AuthLayout />}>
+					<Route
+						path='/signin'
+						element={<SigninForm />}
+					/>
+					<Route
+						path='/signup'
+						element={<SignupForm />}
+					/>
+				</Route>
+				<Route element={<RootLayout />}>
+					<Route
+						index
+						element={<Home />}
+					/>
+				</Route>
+			</Routes>
+		</main>
+	);
+};
+
+export default App;
