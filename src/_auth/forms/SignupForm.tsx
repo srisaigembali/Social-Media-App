2e2178fc786907f60e76789313403b77f9ebe929
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2024-10-07 17:35:15 +0530

    added auth functionality appwrite

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,6 +14,7 @@ import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
 import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
 	const form = useForm<z.infer<typeof SignupValidation>>({
@@ -26,8 +27,9 @@ const SignupForm = () => {
 		},
 	});
 
-	function onSubmit(values: z.infer<typeof SignupValidation>) {
-		console.log(values);
+	async function onSubmit(values: z.infer<typeof SignupValidation>) {
+		const newUser = await createUserAccount(values);
+		console.log(newUser);
 	}
 
 	const isLoading = false;
---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,12 @@
+import { INewUser } from "@/types";
+import { account } from "./config";
+import { ID } from "appwrite";
+
+export const createUserAccount = async (user: INewUser) => {
+	try {
+		const newAccount = await account.create(ID.unique(), user.email, user.password, user.name);
+		return newAccount;
+	} catch (error) {
+		return error;
+	}
+};
