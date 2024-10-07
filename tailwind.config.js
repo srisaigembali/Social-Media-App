b2110fa3516997ea57856aa5c983da92ab5b2b4d
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2024-10-07 16:50:20 +0530

    added shadcn, signup page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
 	darkMode: ["class"],
 	content: [
 		"./pages/**/*.{ts,tsx}",
@@ -57,5 +59,5 @@ export default {
 			},
 		},
 	},
-	plugins: [import("tailwindcss-animate")],
+	plugins: [require("tailwindcss-animate")],
 };
